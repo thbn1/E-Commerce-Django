8e08a206c6b79ed7537fb94c31a6f8ba2d30eba4
taha bayhan <112399497+thbn1@users.noreply.github.com>
2023-09-28 18:52:42 +0300

    for vercel

--- stageproject/settings.py
+++ stageproject/settings.py
@@ -129,8 +129,7 @@ STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
-STATIC_ROOT = '/webapp/static/'
-
+STATIC_ROOT =  os.path.join(BASE_DIR, 'webapp/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
