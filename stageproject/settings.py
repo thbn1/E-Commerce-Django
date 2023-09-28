9a9603a1b404d69bf8dbb7e08dd915708195ef94
taha bayhan <112399497+thbn1@users.noreply.github.com>
2023-09-28 21:37:57 +0300

    Update settings.py

--- stageproject/settings.py
+++ stageproject/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 
 
