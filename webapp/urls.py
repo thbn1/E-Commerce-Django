8d2437e67c34710c2f27ff32136e11e63cc861d5
taha bayhan <112399497+thbn1@users.noreply.github.com>
2023-09-28 21:47:11 +0300

    Update urls.py

--- webapp/urls.py
+++ webapp/urls.py
@@ -41,7 +41,9 @@ urlpatterns = [
 from django.conf import settings
 from django.conf.urls.static import static
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
+
+urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
  
