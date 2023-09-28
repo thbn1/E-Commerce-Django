4774984457cb3c50a70f5c7772bb82f431b47db4
taha bayhan <112399497+thbn1@users.noreply.github.com>
2023-09-28 21:36:45 +0300

    Update settings.py

--- stageproject/settings.py
+++ stageproject/settings.py
@@ -82,14 +82,7 @@ WSGI_APPLICATION = 'stageproject.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '1',
-        'HOST': 'localhost',
-        'PORT': '5432',        
-    }
+
 }
 
 
@@ -115,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'tr'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Europe/Istanbul'
+TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
@@ -128,9 +121,9 @@ USE_TZ = True
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-STATIC_URL = 'static/'
-STATICFILES_DIRS = BASE_DIR / "webapp"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'webapp/static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
