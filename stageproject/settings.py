271c597aa7b3e0ee013691fd8ea8258a4aa69785
taha bayhan <112399497+thbn1@users.noreply.github.com>
2023-09-28 18:54:27 +0300

    Update settings.py

--- stageproject/settings.py
+++ stageproject/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'webapp/static'),
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
