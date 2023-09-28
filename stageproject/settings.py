8ebfd6d52ac98a71ed9fd47b558d60e7836c3c67
taha bayhan <112399497+thbn1@users.noreply.github.com>
2023-09-28 21:45:37 +0300

    Update settings.py

--- stageproject/settings.py
+++ stageproject/settings.py
@@ -124,7 +124,6 @@ MEDIA_URL = '/media/'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
