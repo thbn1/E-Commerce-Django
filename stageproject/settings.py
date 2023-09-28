153d3545bd9a47e38c98e16b3056557665806ea4
taha bayhan <112399497+thbn1@users.noreply.github.com>
2023-09-28 18:46:44 +0300

    for vercel

--- stageproject/settings.py
+++ stageproject/settings.py
@@ -128,7 +128,8 @@ USE_TZ = True
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATIC_ROOT = '/webapp/static/'
 
 
 # Default primary key field type
